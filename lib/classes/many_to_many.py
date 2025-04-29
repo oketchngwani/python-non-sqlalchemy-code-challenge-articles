63bfa60ca4ea670f7669d6a2d8711a594e0a7f6c
Oketch <oketchngwani@gmail.com>
2025-04-30 05:27:21 +0900

    artilcles

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,45 @@
 class Article:
+     all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if hasattr(self, '_title'):
+            raise AttributeError("Title cannot be changed after initialization")
+        if not isinstance(value, str):
+            raise ValueError("Title must be a string")
+        if len(value) < 5 or len(value) > 50:
+            raise ValueError("Title must be between 5 and 50 characters")
+        self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if not isinstance(value, Author):
+            raise ValueError("Author must be an instance of Author")
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        if not isinstance(value, Magazine):
+            raise ValueError("Magazine must be an instance of Magazine")
+        self._magazine = value
         
 class Author:
     def __init__(self, name):
@@ -36,3 +73,4 @@ class Magazine:
 
     def contributing_authors(self):
         pass
+    
