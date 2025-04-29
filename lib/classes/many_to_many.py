0517ece98771597995d5c16ac8f55ccf46282147
Oketch <oketchngwani@gmail.com>
2025-04-30 05:40:18 +0900

    magazines

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -74,3 +74,67 @@ class Author:
         categories = [magazine.category for magazine in self.magazines()]
         unique_categories = list(set(categories))
         return unique_categories if unique_categories else None
+class Magazine:
+    all = []
+
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if len(value) < 2 or len(value) > 16:
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Category must be a string")
+        if len(value) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._category = value
+
+    def articles(self):
+        return [article for article in Article.all if article.magazine == self]
+
+    def contributors(self):
+        authors = [article.author for article in self.articles()]
+        return list(set(authors))
+
+    def article_titles(self):
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
+
+    def contributing_authors(self):
+        author_counts = {}
+        for article in self.articles():
+            author = article.author
+            author_counts[author] = author_counts.get(author, 0) + 1
+        result = [author for author, count in author_counts.items() if count > 2]
+        return result if result else None
+
+    @classmethod
+    def top_publisher(cls):
+        if not Article.all:
+            return None
+        magazine_counts = {}
+        for article in Article.all:
+            mag = article.magazine
+            magazine_counts[mag] = magazine_counts.get(mag, 0) + 1
+        if not magazine_counts:
+            return None
+        max_count = max(magazine_counts.values())
+        top_mags = [mag for mag, count in magazine_counts.items() if count == max_count]
+        return top_mags[0] if top_mags else None
